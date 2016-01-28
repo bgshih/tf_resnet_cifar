Simple data normalization scheme; Fix weight initialization scheme; Model architecture changes

src/create_dataset.py
@@ -32,41 +32,32 @@ def create_dataset():
                 }))
             writer.write(example.SerializeToString())
 
-    # data_root = '/home/bgshi/research/datasets/cifar-10-batches-py'
-    # train_images = np.zeros((50000,3072), dtype=np.float32)
-    # trian_labels = np.zeros((50000,), dtype=np.int32)
-    # for i in xrange(5):
-    #     data_batch = joblib.load(os.path.join(data_root, 'data_batch_%d' % (i+1)))
-    #     train_images[10000*i:10000*(i+1)] = data_batch['data'].astype(np.float32)
-    #     trian_labels[10000*i:10000*(i+1)] = np.asarray(data_batch['labels'], dtype=np.int32)
-    # train_images = np.reshape(train_images, [50000,3,32,32])
-    # train_images = np.transpose(train_images, axes=[0,2,3,1])
-    # images_mean = np.mean(train_images, axis=0)
-    # train_images -= images_mean
-    # train_images /= 256.0
-    # train_set = {'images': train_images, 'labels': trian_labels}
-    # save_to_records('../data/cifar10/train.tf', train_images, trian_labels)
+    data_root = '/home/bgshi/research/datasets/cifar-10-batches-py'
+    train_images = np.zeros((50000,3072), dtype=np.float32)
+    trian_labels = np.zeros((50000,), dtype=np.int32)
+    for i in xrange(5):
+        data_batch = joblib.load(os.path.join(data_root, 'data_batch_%d' % (i+1)))
+        train_images[10000*i:10000*(i+1)] = data_batch['data'].astype(np.float32)
+        trian_labels[10000*i:10000*(i+1)] = np.asarray(data_batch['labels'], dtype=np.int32)
+    train_images = np.reshape(train_images, [50000,3,32,32])
+    train_images = np.transpose(train_images, axes=[0,2,3,1])
+    images_mean = np.mean(train_images, axis=0)
+    data_std = np.std(train_images)
+    train_images -= images_mean
+    train_images /= data_std
+    train_set = {'images': train_images, 'labels': trian_labels}
+    save_to_records('../data/cifar10/train_simple_norm.tf', train_images, trian_labels)
 
-    # data_batch = joblib.load(os.path.join(data_root, 'test_batch'))
-    # test_images = data_batch['data'].astype(np.float32)
-    # test_images = np.reshape(test_images, [10000,3,32,32])
-    # test_images = np.transpose(test_images, axes=[0,2,3,1])
-    # test_images -= images_mean
-    # test_images /= 256.0
-    # test_labels = np.asarray(data_batch['labels'], dtype=np.int32)
-    # test_set = {'images': test_images, 'labels': test_labels}
-    # save_to_records('../data/cifar10/test.tf', test_images, test_labels)
-
-    train_images = loadmat('../data/train_data.mat')['x']
-    train_images = np.ascontiguousarray(np.transpose(train_images, axes=[0,2,3,1]))
-    train_labels = loadmat('../data/train_label.mat')['x'].astype(np.int64) - 1
-    test_images = loadmat('../data/test_data.mat')['x']
-    test_images = np.ascontiguousarray(np.transpose(test_images, axes=[0,2,3,1]))
-    test_labels = loadmat('../data/test_label.mat')['x'].astype(np.int64) - 1
-
-    save_to_records('../data/cifar10/train_cstnorm.tf', train_images, train_labels)
-    save_to_records('../data/cifar10/test_cstnorm.tf', test_images, test_labels)
-
+    data_batch = joblib.load(os.path.join(data_root, 'test_batch'))
+    test_images = data_batch['data'].astype(np.float32)
+    test_images = np.reshape(test_images, [10000,3,32,32])
+    test_images = np.transpose(test_images, axes=[0,2,3,1])
+    test_images -= images_mean
+    test_images /= data_std
+    test_labels = np.asarray(data_batch['labels'], dtype=np.int32)
+    test_set = {'images': test_images, 'labels': test_labels}
+    save_to_records('../data/cifar10/test_simple_norm.tf', test_images, test_labels)
+    
 
 if __name__ == '__main__':
     create_dataset()

src/main_train.py
@@ -13,9 +13,9 @@ import model_resnet as m
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('load_dir', '', '')
-tf.app.flags.DEFINE_integer('residual_net_n', 5, '')
-tf.app.flags.DEFINE_string('train_tf_path', '../data/cifar10/train_cstnorm.tf', '')
-tf.app.flags.DEFINE_string('val_tf_path', '../data/cifar10/test_cstnorm.tf', '')
+tf.app.flags.DEFINE_integer('residual_net_n', 18, '')
+tf.app.flags.DEFINE_string('train_tf_path', '../data/cifar10/train_simple_norm.tf', '')
+tf.app.flags.DEFINE_string('val_tf_path', '../data/cifar10/test_simple_norm.tf', '')
 tf.app.flags.DEFINE_integer('train_batch_size', 128, '')
 tf.app.flags.DEFINE_integer('val_batch_size', 100, '')
 tf.app.flags.DEFINE_float('weight_decay', 1e-4, 'Weight decay')
@@ -76,15 +76,16 @@ def train_and_val():
         # train loop
         tf.train.start_queue_runners(sess=sess)
         curr_lr = 0.0
-        lr_scale = 1.0
         for step in xrange(FLAGS.max_steps):
             # set learning rate manually
-            if step <= 32000:
-                _lr = lr_scale * 1e-1
+            if step <= 2000:
+                _lr = 1e-2
+            elif step <= 32000:
+                _lr = 1e-1
             elif step <= 48000:
-                _lr = lr_scale * 1e-2
+                _lr = 1e-2
             else:
-                _lr = lr_scale * 1e-3
+                _lr = 1e-3
             if curr_lr != _lr:
                 curr_lr = _lr
                 print('Learning rate set to %f' % curr_lr)

src/model_resnet.py
@@ -32,7 +32,7 @@ def conv2d(x, n_in, n_out, k, s, p='SAME', bias=False, scope='conv'):
     with tf.variable_scope(scope):
         kernel = tf.Variable(
             tf.truncated_normal([k, k, n_in, n_out],
-                stddev=math.sqrt(2/(k*k*n_out))),
+                stddev=math.sqrt(2/(k*k*n_in))),
             name='weight')
         tf.add_to_collection('weights', kernel)
         conv = tf.nn.conv2d(x, kernel, [1,s,s,1], padding=p)
@@ -64,7 +64,7 @@ def batch_norm(x, n_out, phase_train, scope='bn', affine=True):
         tf.add_to_collection('weights', gamma)
 
         batch_mean, batch_var = tf.nn.moments(x, [0,1,2], name='moments')
-        ema = tf.train.ExponentialMovingAverage(decay=0.999)
+        ema = tf.train.ExponentialMovingAverage(decay=0.99)
         ema_apply_op = ema.apply([batch_mean, batch_var])
         ema_mean, ema_var = ema.average(batch_mean), ema.average(batch_var)
         def mean_var_with_update():
@@ -90,8 +90,8 @@ def residual_block(x, n_in, n_out, subsample, phase_train, scope='res_block'):
         y = batch_norm(y, n_out, phase_train, scope='bn_1')
         y = tf.nn.relu(y, name='relu_1')
         y = conv2d(y, n_out, n_out, 3, 1, 'SAME', True, scope='conv_2')
-        y = y + shortcut
         y = batch_norm(y, n_out, phase_train, scope='bn_2')
+        y = y + shortcut
         y = tf.nn.relu(y, name='relu_2')
     return y
 